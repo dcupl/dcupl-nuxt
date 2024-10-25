29ba43c3b70d56736b0a5e7a2e6737bd63579627
Dominik Strasser <dominik.strasser@dcupl.com>
2024-10-25 22:27:54 +0200

    chore: add import and types to NitroApp, rename

--- src/runtime/server/services/useDcuplServerInstance.ts
+++ src/runtime/server/services/useDcuplServerInstance.ts
@@ -1,22 +1,30 @@
 import type { H3Event } from 'h3'
 import type { Dcupl } from '@dcupl/core'
 import { DcuplInstance } from '../../../dcupl/dcupl.instance'
+import { useNitroApp, useRuntimeConfig } from '#imports'
+
+declare module 'nitropack' {
+  interface NitroApp {
+    _dcuplNitroInstance: DcuplInstance
+  }
+}
 
 export const useDcuplServerInstance: (event: H3Event) => Promise<Dcupl> = async (event: H3Event) => {
   if (!event.context._dcupl) {
-    const nitroApp = useNitroApp() as { _dcuplSession: DcuplInstance }
-    if (!nitroApp._dcuplSession) {
-      const { dcupl } = useRuntimeConfig().public
-      const dcuplSession = new DcuplInstance('nitro_instance', dcupl)
-      await dcuplSession.init()
-      nitroApp._dcuplSession = dcuplSession
+    const nitroApp = useNitroApp()
+
+    if (!nitroApp._dcuplNitroInstance) {
+      const { dcupl: dcuplConfig } = useRuntimeConfig().public
+      const dcuplNitroInstance = new DcuplInstance('nitro_instance', dcuplConfig)
+      await dcuplNitroInstance.init()
+      nitroApp._dcuplNitroInstance = dcuplNitroInstance
     }
     // Since this is stored on Server Side, maybe add a request to "update" the context instaed of validating on every request
-    // if (await nitroApp._dcuplSession.shouldUpdate()) {
-    //   nitroApp._dcuplSession.init() // update async
+    // if (await nitroApp._dcuplNitroInstance.shouldUpdate()) {
+    //   nitroApp._dcuplNitroInstance.init() // update async
     // }
     //
-    return nitroApp._dcuplSession.dcupl
+    return nitroApp._dcuplNitroInstance.dcupl
   }
   else {
     return event.context._dcupl as Dcupl
